14cbefdb626d215d177ba9578e4c3b43aaf12d48
정성훈 <junghunsung01@gmail.com>
2023-06-28 19:59:12 +0900

    fix: import 에러 제거

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
