26be8ce64c8ad74c57f677730302916cd7965f57
정성훈 <junghunsung01@gmail.com>
2024-03-05 14:23:41 +0900

    feat: 헤더 레이아웃 수정

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,26 +5,21 @@ import styled from "styled-components"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
 
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
+  const header = (
+    <Link className="header-link-home" to="/">
+      {title}
+    </Link>
+  )
 
   return (
     <Container className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
-      <main>{children}</main>
+      <header className="global-header">
+        <div className="global-header-center">{header}</div>
+      </header>
+      <div className="global-center">
+        <main className="global-main">{children}</main>
+      </div>
     </Container>
   )
 }
